e8ece09253aca2808d328caa025eb6718e541971
AlyaShawqi <168932687+AlyaShawqi@users.noreply.github.com>
2025-02-21 18:44:00 +0300

    bb

--- my_notes_itcs444/lib/tryAss1.dart
+++ my_notes_itcs444/lib/tryAss1.dart
@@ -17,77 +17,111 @@ class _Ass1State extends State<Ass1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Row(
-          children: [
-            Icon(Icons.home),
-            SizedBox(width: 20.0),
-            Text("Laptops"),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Row(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 40.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: BeveledRectangleBorder(
-                          borderRadius: BorderRadius.zero),
-                      backgroundColor: getButtonColor(1)),
-                  onPressed: () {
-                    setState(() {
-                      _selectedIndex = 1;
-                    });
-                  },
-                  child: Text("Show Part 1"),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 40.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape:
-                        RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-                    backgroundColor: getButtonColor(2),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _selectedIndex = 2;
-                    });
-                  },
-                  child: Text("Show Part 2"),
-                ),
-              ),
+              Icon(Icons.home),
+              SizedBox(width: 20.0),
+              Text("Laptops"),
             ],
           ),
-          SizedBox(height: 20),
-          Expanded(
-            child: _selectedIndex == 1
-                ? PartOne()
-                : _selectedIndex == 2
-                    ? PartTwo()
-                    : Center(child: Text("Click a button to show content")),
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 40.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: BeveledRectangleBorder(
+                            borderRadius: BorderRadius.zero),
+                        backgroundColor: getButtonColor(1)),
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 1;
+                      });
+                    },
+                    child: Text(
+                      "Show Part 1",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                      backgroundColor: getButtonColor(2),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 2;
+                      });
+                    },
+                    child: Text("Show Part 2",
+                        style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: _selectedIndex == 1
+                  ? PartOne()
+                  : _selectedIndex == 2
+                      ? PartTwo()
+                      : Center(child: Text("Click a button to show content")),
+            ),
+          ],
+        ),
+        floatingActionButton: SizedBox(
+          //we used sized box to put the floating button in it and then control the size of the floating button
+          width: 100,
+          height: 100,
+          child: FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                        title: const Text("Alert Dialog"),
+                        content: const Text("Welcome Alya"),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.pop(context, "Thanks"),
+                              child: Text("Thanks")),
+                          TextButton(
+                              onPressed: () => Navigator.pop(context, "Cancle"),
+                              child: Text("Cancle"))
+                        ],
+                      ));
+            },
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.volume_up,
+              size: 50,
+              color: const Color.fromARGB(255, 23, 124, 0),
+            ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
 
 class PartOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "This is Part 1",
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Column(children: [
+      TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Brand Name:',
+        ),
       ),
-    );
+    ]);
   }
 }
 
